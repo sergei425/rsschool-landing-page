06e6a7166c7921b4e5cdbf59c46e1f8e0d37280d
sergei404 <kvakazuabr@yandex.ru>
2026-09-20 20:07:02 +0300

    feat: add base styles and header styles

--- vite.conffig.js
+++ vite.conffig.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        menu: resolve(__dirname, 'menu.html'),
+        main: resolve(__dirname, "index.html"),
+        menu: resolve(__dirname, "menu.html"),
       },
     },
   },
-})
+});
